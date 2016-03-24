6d751d7f0532d6c649aec0918f3aabc479b37a04
Nick Walker <nick.walker@puppetlabs.com>
2016-03-24 15:16:03 -0700

    Copy public key as well as private into VM

--- puppet_code/ssh_key_placement.pp
+++ puppet_code/ssh_key_placement.pp
@@ -5,3 +5,10 @@ file { '/etc/puppetlabs/puppetserver/code_manager.key':
   owner  => 'pe-puppet',
   group  => 'pe-puppet',
 }
+
+file { '/etc/puppetlabs/puppetserver/code_manager.key.pub':
+  ensure => present,
+  source => '/vagrant/ssh_key/code_manager.key.pub',
+  owner  => 'pe-puppet',
+  group  => 'pe-puppet',
+}
