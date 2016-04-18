b7a4726335e1aa2e3b2c8ea41ffd6d917ad4e2d4
Nick Walker <nick.walker@puppetlabs.com>
2016-04-18 15:16:25 -0700

    Update the stack to use the new r10k private key location

--- puppet_code/ssh_key_placement.pp
+++ puppet_code/ssh_key_placement.pp
@@ -1,12 +1,12 @@
 
-file { '/etc/puppetlabs/puppetserver/code_manager.key':
+file { [ '/etc/puppetlabs/puppetserver/code_manager.key', '/etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa' ]:
   ensure => present,
   source => '/vagrant/ssh_key/code_manager.key',
   owner  => 'pe-puppet',
   group  => 'pe-puppet',
 }
 
-file { '/etc/puppetlabs/puppetserver/code_manager.key.pub':
+file { [ '/etc/puppetlabs/puppetserver/code_manager.key.pub', '/etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa.pub' ] :
   ensure => present,
   source => '/vagrant/ssh_key/code_manager.key.pub',
   owner  => 'pe-puppet',
